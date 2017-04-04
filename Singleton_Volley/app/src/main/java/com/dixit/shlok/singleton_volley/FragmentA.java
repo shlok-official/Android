d337d46f245a3d81dd687d641edf95a91c652ed7
Shlok Dixit <shlok.official@gmail.com>
2017-04-04 00:41:58 -0400

    created singleton

--- Singleton_Volley/app/src/main/java/com/dixit/shlok/singleton_volley/FragmentA.java
+++ Singleton_Volley/app/src/main/java/com/dixit/shlok/singleton_volley/FragmentA.java
@@ -7,6 +7,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 /**
  * Created by ShlokDixit on 3/28/17.
  */
@@ -15,6 +22,19 @@ public class FragmentA extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_a,container,false);
+        View layout =  inflater.inflate(R.layout.fragment_a,container,false);
+        RequestQueue requestQueue = Volley.newRequestQueue(getActivity());
+        StringRequest request = new StringRequest(Request.Method.GET, "", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+                return layout;
     }
 }
